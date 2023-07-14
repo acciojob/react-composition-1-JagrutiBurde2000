ebafbe45b7c72adf6d6d569165a800dcc61a3da3
John Doe <johndoe@email.com>
2023-07-14 18:38:43 +0530

    good

--- src/components/App.js
+++ src/components/App.js
@@ -30,12 +30,12 @@ const Tabs = ({ tabs }) => {
 // Usage example
 const App = () => {
   const tabs = [
-    { title: 'Tab 1', content: 'This is content for Tab 1' },
-    { title: 'Tab 2', content: 'This is content for Tab 2' },
-    { title: 'Tab 3', content: 'This is content for Tab 3' },
+    { title: 'Tab 1', content: 'This is content for Tab 1.' },
+    { title: 'Tab 2', content: 'This is content for Tab 2.' },
+    { title: 'Tab 3', content: 'This is content for Tab 3.' },
   ];
 
-  
+
   return (
     <div>
    
