5ce3fe3d07c3059c6c2d15806fd3f14688491d56
John Doe <johndoe@email.com>
2023-08-13 13:44:15 +0530

    go

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,15 @@
 
 import React, { useState } from 'react';
 import './../styles/App.css';
-
-const Tabs = ({ tabs }) => {
-  const [activeTab, setActiveTab] = useState(0);
-
-  const handleTabClick = (index) => {
-    setActiveTab(index);
-  };
-  
-  
-
-  return (
-    <div>
-      <ul>
-        {tabs.map((tab, index) => (
-          <li
-            key={index}
-            onClick={() => handleTabClick(index)}
-            style={{ cursor: 'pointer' }}
-          >
-            {tab.title}
-          </li>
-        ))}
-      </ul>
-      <div>{tabs[activeTab].content}</div>
-    </div>
-  );
-};
-
+import Tabs from './Tabs';
+const tabs = [
+  { title: 'Tab 1', content: 'Tab 1.' },
+  { title: 'Tab 2', content: 'Tab 2.' },
+  { title: 'Tab 3', content: 'Tab 3.' },
+];
 // Usage example
 const App = () => {
-  const tabs = [
-    { title: 'Tab 1', content: 'This is content for Tab 1.' },
-    { title: 'Tab 2', content: 'This is content for Tab 2.' },
-    { title: 'Tab 3', content: 'This is content for Tab 3.' },
-  ];
+
 
   return (
     <div>
---
+++ src/components/Tabs.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from 'react';
+
+
+const Tabs = ({ tabs }) => {
+    const [tabClicked, setTabClicked] = useState("");
+
+  
+    const handleTabClick = (content) => {
+        setTabClicked(content);
+    };
+    
+    
+  
+    return (
+      <div>
+        <ul>
+          {
+          tabs.map((value) => 
+            <li onClick={() => handleTabClick(value.content)}>
+              {value.title}
+            </li>
+          )
+          }
+        </ul>
+
+{
+
+         tabClicked  && <p>This is content for {tabClicked}</p>
+        
+}
+      </div>
+    );
+  };
+  
+  export default Tabs;
